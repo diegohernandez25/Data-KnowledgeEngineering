65bee143a4696caf76c1a20135e7ba63ba8b2400
Diego Caldeira Hernandez <diego.hernandez.c25@gmail.com>
2018-10-19 17:24:53 +0100

    Removed Template's unecessary content. Associated each button with Django and addedd Files with the correspondent url for each application. Added a Right Bar for each application

--- uaggregator/views.py
+++ uaggregator/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 # Create your views here.
+
 
 def index(request):
     return render(request, 'index.html',{})
+
+    request.build_absolute_uri
+
+def news(request):
+    return render(request,'news.html',{})
+    #return HttpRequest.build_absolute_uri('news.html')
+
+def meteorology(request):
+    return render(request,'meteorology.html',{})
+
+def schedule(request):
+    return render(request,'schedule.html',{})
+
+def room(request):
+    return render(request,'room.html',{})
+
+def cantine(request):
+    return render(request,'cantine.html',{})
+
+def parkinglot(request):
+    return render(request,'parkinglot.html',{})
