93b983dcd59e9dd8d5c291b2f199b61e0eaee66e
Diego Caldeira Hernandez <diego.hernandez.c25@gmail.com>
2018-11-06 19:28:27 +0000

    Divided by zero fixed for real

--- stubs/uaservices.py
+++ stubs/uaservices.py
@@ -351,7 +351,8 @@ class ScheduleMaker(XMLService):
 		print(self.schedules[1])	
 	def calculate_timeoffset(self, start, end):
 		_tmp = end - start
-		if(_tmp==0.30) return 1
+		if(int(_tmp*100)==30): 
+			return 1
 		_tmp = int(_tmp)*2 + (1 if _tmp%int(_tmp) else 0)
 		return _tmp
 	
